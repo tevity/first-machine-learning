e93f694990e49655b1ae89feae907d6d6392b726
Kevin Gurton <kevin.gurton@gmail.com>
2020-05-20 17:42:20 +0100

    Added some analysis

--- load_data.py
+++ load_data.py
@@ -3,4 +3,9 @@ import pandas
 filename = 'dataset/forestfires.csv'
 names = ['X', 'Y', 'month', 'day', 'FFMC', 'DMC', 'DC', 'ISI', 'temp', 'RH', 'wind', 'rain', 'area']
 df = pandas.read_csv(filename, names=names)
-print(pandas.isnull(df))
+df.month.replace(('jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'), (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), inplace=True)
+df.day.replace(('mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'), (1, 2, 3, 4, 5, 6, 7), inplace=True)
+print(df.head(1))
+print(df.dtypes)
+print(df.describe())
+print(df.corr(method='pearson'))
